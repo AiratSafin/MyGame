42ce51b420705773f0a90883b34cad17cf100d3f
Ayrat <74006664+AiratSafin@users.noreply.github.com>
2022-01-15 20:45:37 +0300

    добавил exe

---
+++ dist/data/board/tiled/plant repack.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="plant repack" tilewidth="16" tileheight="16" tilecount="676" columns="26">
+ <image source="plant repack.png" width="416" height="416"/>
+</tileset>
